c3a524587d3c5f41119c4eedb9d1330c6c8fac9a
Lucitar <victorlu3391@gmail.com>
2026-02-27 16:39:01 -0300

    feat: add RabbitMQ config

---
+++ src/main/java/com/notifications/notification_service/config/RabbitMQConfig.java
@@ -0,0 +1,43 @@
+package com.notifications.notification_service.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE = "task.notifications";
+    public static final String EXCHANGE = "task.exchange";
+    public static final String ROUTING_KEY = "task.created";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, true); // true = durable, sobrevive a restart
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
